0b0b518d230e37480435120a2dab1d0bc6753957
Aktilek0123 <aktilek0123@gmail.com>
2023-06-23 16:08:26 +0600

    2

---
+++ src/pages/ProductPage/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCard from "../../components/Product/ProductCard/ProductCard";
+
+const Products = () => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+};
+
+export default Products;
