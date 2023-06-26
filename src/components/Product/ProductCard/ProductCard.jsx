4ef5d4997f9b739a526d0da443c1a144814f97b1
Aidar <abdyldaev.aidar@gmail.com>
2023-06-26 17:40:12 +0600

    first update

--- src/components/Product/ProductCard/ProductCard.jsx
+++ src/components/Product/ProductCard/ProductCard.jsx
@@ -3,60 +3,67 @@ import "./ProductCard.css"
 
 const ProductCard = () => {
   return (
-  <div className="product_cards">
-    <div className="card_item">
-    {/* <div className="card_border"></div> */}
-      <img 
-      src="https://www.villafranciacorta.it/ecommerce/wp-content/uploads/2022/11/SelezioneBrut_2011.png" 
-      alt="nothing but a bottle of wine" 
-      />
-      <h3>SELEZIONE 2011</h3>
-      <div className="card_item_text">
-        {/* <h4>SOLD OUT</h4> */}
-        <h5>FRANCIACORTA DOCG</h5>
+  <div className="productCard_container">
+    <h2>FRANCIACORTA</h2>
+    <div className="productCard_wrapper">
+      <div className="sidebar">
+        <div className="slide_wrapper">
+          <div className="relative">
+            <span>ALL</span>
+          </div>
+          <div className="absolute">
+            <span>ALL</span>
+          </div>
+        </div>
+          <div className="slide_wrapper">
+            <div className="relative">
+              <span>GRAPPE</span>
+            </div>
+            <div className="absolute">
+              <span>GRAPPE</span>
+            </div>
+          </div>
+          <div className="slide_wrapper">
+            <div className="relative">
+              <span>WINES</span>
+            </div>
+            <div className="absolute">
+              <span>WINES</span>
+            </div>
+          </div>
+          <div className="slide_wrapper">
+            <div className="relative">
+              <span>FRANCIACORTA</span>
+            </div>
+            <div className="absolute">
+              <span>FRANCIACORTA</span>
+            </div>
+          </div>
       </div>
-      <button>DISCOVER</button>
-    </div>;
-    <div className="card_item">
-    {/* <div className="card_border"></div> */}
-      <img 
-      src="https://www.villafranciacorta.it/ecommerce/wp-content/uploads/2022/11/SelezioneBrut_2011.png" 
-      alt="nothing but a bottle of wine" 
-      />
-      <h3>SELEZIONE 2011</h3>
-      <div className="card_item_text">
-        {/* <h4>SOLD OUT</h4> */}
-        <h5>FRANCIACORTA DOCG</h5>
+      <div className="product_cards">
+        <div className="card_item">
+          <img 
+          src="https://www.villafranciacorta.it/ecommerce/wp-content/uploads/2022/11/SelezioneBrut_2011.png" 
+          alt="nothing but a bottle of wine" 
+          />
+          <div className="slide_wrapper">
+            <div className="relative">
+              <h3>SELEZIONE 2011</h3>
+            </div>
+            <div className="absolute">
+              <h3>SELEZIONE 2011</h3>
+            </div>
+          </div>
+          <div className="card_item_text">
+            {/* <h4>SOLD OUT</h4> */}
+            <h5>FRANCIACORTA DOCG</h5>
+          </div>
+          <button>DISCOVER</button>
+        </div>
       </div>
-      <button>DISCOVER</button>
-    </div>;
-    <div className="card_item">
-    {/* <div className="card_border"></div> */}
-      <img 
-      src="https://www.villafranciacorta.it/ecommerce/wp-content/uploads/2022/11/SelezioneBrut_2011.png" 
-      alt="nothing but a bottle of wine" 
-      />
-      <h3>SELEZIONE 2011</h3>
-      <div className="card_item_text">
-        {/* <h4>SOLD OUT</h4> */}
-        <h5>FRANCIACORTA DOCG</h5>
-      </div>
-      <button>DISCOVER</button>
-    </div>;
-    <div className="card_item">
-    {/* <div className="card_border"></div> */}
-      <img 
-      src="https://www.villafranciacorta.it/ecommerce/wp-content/uploads/2022/11/SelezioneBrut_2011.png" 
-      alt="nothing but a bottle of wine" 
-      />
-      <h3>SELEZIONE 2011</h3>
-      <div className="card_item_text">
-        {/* <h4>SOLD OUT</h4> */}
-        <h5>FRANCIACORTA DOCG</h5>
-      </div>
-      <button>DISCOVER</button>
-    </div>;
-  </div>)
+    </div>
+  </div>
+  )
 };
 
 export default ProductCard;
