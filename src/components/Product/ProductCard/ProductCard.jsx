519f79a19d67c9dfa024f868553147d07d926c97
Aktilek0123 <aktilek0123@gmail.com>
2023-07-03 16:15:26 +0600

    14

--- src/components/Product/ProductCard/ProductCard.jsx
+++ src/components/Product/ProductCard/ProductCard.jsx
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../store/products/productAction";
 import { useNavigate } from "react-router-dom";
 
-const ProductCard = ({ searchProduct }) => {
-  // const { products } = useSelector((state) => state.products);
+const ProductCard = ({ currentData }) => {
+  const { products } = useSelector((state) => state.products);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,27 +15,31 @@ const ProductCard = ({ searchProduct }) => {
 
   return (
     <div className="product_cards_container">
-      {searchProduct.map((item) => (
-        <div className="product_cards" key={item.id}>
-          <div className="card_item">
-            <img src={item?.image} alt="nothing but a bottle of wine" />
-            <div className="slide_wrapper" id="font_nanum">
-              <div className="relative">
-                <h3>{item?.name}</h3>
+      {products ? (
+        currentData().map((item) => (
+          <div className="product_cards" key={item.id}>
+            <div className="card_item">
+              <img src={item?.image} alt="nothing but a bottle of wine" />
+              <div className="slide_wrapper" id="font_nanum">
+                <div className="relative">
+                  <h3>{item?.name}</h3>
+                </div>
+                <div className="absolute">
+                  <h3>{item?.name}</h3>
+                </div>
               </div>
-              <div className="absolute">
-                <h3>{item?.name}</h3>
+              <div className="card_item_text">
+                <h5 id="font_pathway">{item?.sort}</h5>
               </div>
+              <button onClick={() => navigate(`/details/${item?.id}`)}>
+                DISCOVER
+              </button>
             </div>
-            <div className="card_item_text">
-              <h5 id="font_pathway">{item?.sort}</h5>
-            </div>
-            <button onClick={() => navigate(`/details/${item?.id}`)}>
-              DISCOVER
-            </button>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
