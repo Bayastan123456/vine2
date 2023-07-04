5a44576e14f0697394f47d7cf7cc85de69a0d910
Bayastan <abakirovbayastan@gmail.com>
2023-07-04 11:22:24 +0300

    dwdwd

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
 import { productReducer } from "./products/productSlice";
-<<<<<<< HEAD
-import { cartReducers } from "./cart/cartSlice";
-=======
-import commentReducer from './comment/commentSlice'
->>>>>>> dc33b8155a21c88b04022330cd04f0d5f0a7248a
+import commentReducer from "./comment/commentSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
-<<<<<<< HEAD
-    cart: cartReducers,
-=======
     comments: commentReducer,
->>>>>>> dc33b8155a21c88b04022330cd04f0d5f0a7248a
   },
 });
