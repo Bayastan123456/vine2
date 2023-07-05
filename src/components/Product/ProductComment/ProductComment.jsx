6803676bff0f87e597d5868bd8fe7bfb2fd673d2
VladRastortguev <128030167+VladRastortguev@users.noreply.github.com>
2023-07-05 15:12:26 +0600

    likes

--- src/components/Product/ProductComment/ProductComment.jsx
+++ src/components/Product/ProductComment/ProductComment.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import './ProductComment.css'
+import { useSelector, useDispatch } from "react-redux";
+import { likeComment } from "../../../store/comment/commentSlice";
+import "./ProductComment.css";
 
 const ProductComments = ({ productId }) => {
   const comments = useSelector((state) => state.comments[productId] || []);
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -13,7 +15,16 @@ const ProductComments = ({ productId }) => {
       ) : (
         <ul className="commentList">
           {comments.map((comment, index) => (
-            <li key={index} className="commentItem">{comment}</li>
+            <li key={index} className="commentItem">
+              {comment.text} (Likes: {comment.likes})
+              <button
+                onClick={() =>
+                  dispatch(likeComment({ productId, commentId: comment.id }))
+                }
+              >
+                Like
+              </button>
+            </li>
           ))}
         </ul>
       )}
@@ -22,3 +33,28 @@ const ProductComments = ({ productId }) => {
 };
 
 export default ProductComments;
+
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import './ProductComment.css'
+
+// const ProductComments = ({ productId }) => {
+//   const comments = useSelector((state) => state.comments[productId] || []);
+
+//   return (
+//     <div>
+//       <h3 className="productCommentsTitel">Comments for Product {productId}</h3>
+//       {comments.length === 0 ? (
+//         <p className="productCommentP">No comments yet.</p>
+//       ) : (
+//         <ul className="commentList">
+//           {comments.map((comment, index) => (
+//             <li key={index} className="commentItem">{comment}</li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ProductComments;
